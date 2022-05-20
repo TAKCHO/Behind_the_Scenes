fa02949888d7e0958cc42dd305cd2879a3ee70d0
Tako <takogiorgobiani97@gmail.com>
2022-05-20 23:22:12 +0400

    scope

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 function calcAge(birthyear) {
   const age = 2022 - birthyear;
@@ -6,6 +6,23 @@ function calcAge(birthyear) {
 
   function printAge() {
     const output = `You are ${age}, born in ${birthyear}`;
+    console.log(output);
+
+    if (birthyear >= 1981 && birthyear <= 1997) {
+      var millenial = true;
+      //   creating New variable with th same name as outer scope's variable
+      const firstName = 'John';
+
+      //   Reassigning outer scope's varible
+      const output = 'NEW OUTPUT!';
+      const str = `oh, you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(add(2, 3)); // works with no strict mode
     console.log(output);
   }
 
