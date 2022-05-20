30dd25cf258fbd4a7cb61671fc200e3f0053450b
Tako <takogiorgobiani97@gmail.com>
2022-05-20 23:08:35 +0400

    m

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-"use strict";
+'use strict';
+
+function calcAge(birthyear) {
+  const age = 2022 - birthyear;
+  //   console.log(firstName);
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthyear}`;
+    console.log(output);
+  }
+
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Tako';
+calcAge(1997);
